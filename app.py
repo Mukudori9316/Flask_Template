2b50f9c61c96726f99fc07fcec116e834a2486ae
Ryota Fukai <ryotaf1116@gmail.com>
2017-02-01 12:25:08 +0900

    add bootstrap

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Hello world!"
+    return "index"
+
+
+@app.route("/hello/")
+def hello():
+    return render_template('hello.html')
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    pass
 
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
